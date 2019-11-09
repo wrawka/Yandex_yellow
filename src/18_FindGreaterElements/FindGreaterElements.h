a3c5dfb5adeacdeff0934af826ce8aeb3993a608
wrawka <wrawka@gmail.com>
2019-11-09 16:27:27 +0300

    [SOLEVED] FindGreaterElements

--- src/18_FindGreaterElements/FindGreaterElements.h
+++ src/18_FindGreaterElements/FindGreaterElements.h
@@ -1,4 +1,15 @@
-//#pragma once
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
 
 template <typename T>
-    vector<T> FindGreaterElements(const set<T>& elements, const T& border);
+vector<T> FindGreaterElements(const set<T>& elements, const T& border) {
+    vector<T> out;
+    auto border_it = find_if(begin(elements), end(elements), [border](T element) {return element > border;});
+    for (auto it = border_it; it != end(elements); it++) {
+        out.push_back(*it);
+    }
+    return out;
+}
