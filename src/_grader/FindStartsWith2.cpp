09474ab022646f8e052133469c87b794ec2d4ba4
wrawka <wrawka@gmail.com>
2019-12-30 16:32:22 +0300

    Update FindStartsWith2.cpp

--- src/_grader/FindStartsWith2.cpp
+++ src/_grader/FindStartsWith2.cpp
@@ -4,6 +4,14 @@ pair<RandomIt, RandomIt> FindStartsWith(
     const string& prefix) {
 	string st = prefix;
 	RandomIt it = lower_bound(range_begin, range_end, st);
-	++st[prefix.size() - 1];
+	for (size_t i = 1; i < prefix.size(); ++i) {
+		if (st[prefix.size() - i] == 122) {
+			++st[prefix.size() - i - 1];
+			st[prefix.size() - i] = 97;
+		} else {
+			++st[prefix.size() - 1];
+			break;
+		}
+	}
 	return make_pair(it, upper_bound(it, range_end, st));
 }
