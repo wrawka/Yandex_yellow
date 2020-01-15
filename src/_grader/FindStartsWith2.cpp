e195ee715082e53273fec686e0f5d2261f3165e9
wrawka <wrawka@gmail.com>
2020-01-15 16:23:59 +0300

    [SOLVED] FindStartsWith P.2

--- src/_grader/FindStartsWith2.cpp
+++ src/_grader/FindStartsWith2.cpp
@@ -4,14 +4,9 @@ pair<RandomIt, RandomIt> FindStartsWith(
     const string& prefix) {
 	string st = prefix;
 	RandomIt it = lower_bound(range_begin, range_end, st);
-	for (size_t i = 1; i < prefix.size(); ++i) {
-		if (st[prefix.size() - i] == 122) {
-			++st[prefix.size() - i - 1];
-			st[prefix.size() - i] = 97;
-		} else {
-			++st[prefix.size() - 1];
-			break;
-		}
+	if (it == range_end) {
+		return {it, it};
 	}
-	return make_pair(it, upper_bound(it, range_end, st));
+	++(*prev(st.end()));
+	return make_pair(it, lower_bound(it, range_end, st));
 }
